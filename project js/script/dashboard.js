4fdda955efaa2d5212c4cbf20869d16b238c29a4
zainabmostajeran <zainab1372m@gmail.com>
2024-08-30 23:05:58 +0330

    pagination

--- project js/script/dashboard.js
+++ project js/script/dashboard.js
@@ -7,7 +7,9 @@ import { errorHandler } from "../libs/errorhandler";
 import { removeSessionToken } from "../libs/session-manager";
 const listSnekears = document.getElementById("listSnekears");
 const btnBrand = document.getElementById("btnBrand");
-// const paginationElement = document.getElementById("pagination");
+const searchInput=document.getElementById("search-input")
+const paginationElement = document.getElementById("pagination");
+
 
 let currentPage = 1;
 let itemsPerPage = 10;
@@ -29,16 +31,14 @@ fetchUserInfo();
 async function fetchSneakerInfo() {
   try {
     const params = {
-      page:currentPage,
-      limit:itemsPerPage,
-      search:currentSearch,
-      brands:selectedBrands,
+      page: currentPage,
+      limit: itemsPerPage,
+      search: currentSearch,
+      brands: selectedBrands,
     };
     const response = await getSneakers(params);
-    let render = response.data.map((el, index) => {
-      return generateRowSneakerInfo(el, index);
-    });
-    listSnekears.innerHTML = render.join("");
+    renderSneakers(response.data);
+    setupPagination(response.totalPages);
     console.log(response);
   } catch (error) {
     errorHandler(error);
@@ -46,13 +46,17 @@ async function fetchSneakerInfo() {
   }
 }
 fetchSneakerInfo();
+function renderSneakers(sneakers) {
+  let render = sneakers.map((el, index) => generateRowSneakerInfo(el, index));
+  listSnekears.innerHTML = render.join("");
+}
 // render sneakersInfo
 function generateRowSneakerInfo(sneaker, index) {
   return `
   <div class="card flex flex-col gap-2 items-center justify-start" data-index="${index}">
-    <div class="rounded-2xl w-40 h-40  overflow-hidden">
+    <a href="#" class="rounded-2xl w-40 h-40  overflow-hidden">
     <img  src="${sneaker.imageURL}"> 
-    </div>
+    </a>
     <div class="flex flex-col gap-2  px-2 items-start justify-center">
    <p class="font-bold max-w-40 text-nowrap overflow-hidden text-lg">${sneaker.name}...</p>
    <p class="font-semibold text-base">$ ${sneaker.price}.00</p>
@@ -67,8 +71,9 @@ async function fetchBrands() {
     let renderBrand = response.map((el, index) => {
       return generateBrand(el, index);
     });
-    btnBrand.innerHTML=`<button class="filter border border-black rounded-full px-7 py-1 font-semibold text-base" data-filter="All">All</button>`
-    + renderBrand.join("");
+    btnBrand.innerHTML =
+      `<button class="filter border border-black rounded-full px-7 py-1 font-semibold text-base" data-filter="All">All</button>` +
+      renderBrand.join("");
     handleBrandFilter();
   } catch (error) {
     errorHandler(error);
@@ -79,7 +84,7 @@ fetchBrands();
 // generateBrand
 function generateBrand(brand, index) {
   return `<button
-  class=" border border-5 border-black  rounded-full px-5 py-1 font-bold text-base " data-index="${index}"
+  class="filter border border-5 border-black  rounded-full px-5 py-1 font-bold text-base " data-index="${index}"
   data-filter="${brand}">${brand}
   </button>`;
 }
@@ -87,30 +92,39 @@ function generateBrand(brand, index) {
 function handleBrandFilter() {
   btnBrand.addEventListener("click", (event) => {
     const filter = event.target.dataset.filter;
-    console.log(filter);
-    if (filter) {
-      selectedBrands=filter==="All"?[]:[filter];
-      //  currentPage = 1;
+    if(filter){
+      const brandButtons=btnBrand.querySelectorAll(".filter")
+      brandButtons.forEach((button)=>{
+        button.classList.remove("text-white","bg-gray-800")
+      })
+      event.target.classList.add("text-white","bg-gray-800")
+      selectedBrands = filter === "All" ? [] : [filter];
+       currentPage = 1;
       fetchSneakerInfo();
     }
   });
 }
-//pagination
-// function setupPagination(totalPages) {
-//   paginationElement.innerHTML = "";
-//   Array.from({ length: totalPages }, (_, i) => i + 1).forEach((pageNumber) => {
-//     const span = document.createElement("span");
-//     span.className = "pagination";
-//     span.innerText = pageNumber;
-//     span.addEventListener("click", () => changePage(pageNumber));
-//     paginationElement.appendChild(span);
-//   });
-// }
-
-// function changePage(page) {
-//   currentPage = page;
-//   fetchSneakerInfo();
-// }
+pagination
+function setupPagination(totalPages) {
+  paginationElement.innerHTML = "";
+  Array.from({ length: totalPages }, (_, i) => i + 1).forEach((pageNumber) => {
+    const span = document.createElement("span");
+    span.classList.add("border","border-gray-400","px-3","py-1")
+    span.innerText = pageNumber;
+    span.addEventListener("click", () => changePage(pageNumber));
+    paginationElement.appendChild(span);
+  });
+}
+function changePage(page) {
+  currentPage = page;
+  fetchSneakerInfo();
+}
+//search
+searchInput.addEventListener("input",()=>{
+  currentSearch = searchInput.value.trim();
+  currentPage = 1;
+  fetchSneakerInfo();
+})
 //say hello
 let sayHello = document.getElementById("sayHello");
 const greeting = ["Good Morning", "Good Afternoon", "Good Evening"];
@@ -127,4 +141,3 @@ document.getElementById("logout-btn").addEventListener("click", () => {
   removeSessionToken();
   window.location.href = "/";
 });
-
